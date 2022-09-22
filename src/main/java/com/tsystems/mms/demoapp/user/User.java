7cf4f42309c883b4efead0e688aff9a309062a2f
joooki82 <jakabtamas82@gmail.com>
2022-09-22 09:39:17 +0200

    extend the existing user entity (DemoUser) with first, surname and gender this attributes have to be not 'null' the gender types should be an 'enum'

--- src/main/java/com/tsystems/mms/demoapp/user/User.java
+++ src/main/java/com/tsystems/mms/demoapp/user/User.java
@@ -2,38 +2,78 @@ package com.tsystems.mms.demoapp.user;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.tsystems.mms.demoapp.model.Gender;
+
 import java.io.Serializable;
 
 @Entity
 @Table(name = "demo_user")
 public class User implements Serializable {
 
-  private static final long serialVersionUID = 1715994813284718249L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id", nullable = false)
-  protected Long id;
+	private static final long serialVersionUID = 1715994813284718249L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	protected Long id;
 
-  @Column(name = "email", nullable = false)
-  private String email;
+	@Column(name = "email", nullable = false)
+	private String email;
 
-  public Long getId() {
-    return id;
-  }
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	@Column(name = "surname", nullable = false)
+	private String surname;
 
-  public String getEmail() {
-    return email;
-  }
+	@Column(name = "gender", nullable = false)
+	@Enumerated(EnumType.ORDINAL)
+	private Gender gender;
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setLastName(String surname) {
+		this.surname = surname;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
 }
